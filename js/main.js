8064c060a511bfa3d58390d26ac7ba8a8149caa4
andreideman <demenkov2001@mail.ru>
2022-04-26 23:58:43 +0300

    SKIA_DIPLOM

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.rev_slider').slick({
+
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed:1000,
+
+  // centerMode: true,
+  // centerPadding: '60px',
+
+  prevArrow: "<img src='/slick/fonts/prev.png' class='prev' alt='1'>",
+    nextArrow: "<img src='/slick/fonts/next.png' class='next' alt='2'>",
+    })
+})

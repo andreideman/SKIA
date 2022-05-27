3e22a1b156b45934d29f57ad4a006487edd3ae9d
andreideman <demenkov2001@mail.ru>
2022-05-27 21:18:41 +0300

    two

--- js/main.js
+++ js/main.js
@@ -6,10 +6,24 @@ $(function () {
   autoplaySpeed: 2000,
   speed:1000,
 
-  // centerMode: true,
-  // centerPadding: '60px',
+  centerMode: true,
+  centerPadding: '250px',
 
   prevArrow: "<img src='/slick/fonts/prev.png' class='prev' alt='1'>",
     nextArrow: "<img src='/slick/fonts/next.png' class='next' alt='2'>",
     })
+})
+
+$(function () {
+  $('.resp_slider').slick({
+
+slidesToShow: 2,
+autoplay: true,
+autoplaySpeed: 2000,
+speed:1000,
+
+
+prevArrow: "<img src='/slick/fonts/prev.png' class='prev' alt='1'>",
+  nextArrow: "<img src='/slick/fonts/next.png' class='next' alt='2'>",
+  })
 })
